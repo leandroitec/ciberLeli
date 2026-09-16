ce032dc822de2cd241b751d0eda6793ad5b2528a
leandroitec <l.odetto@itecriocuarto.org.ar>
2026-09-15 21:10:20 -0300

    cambio de vistas de usuario de apiview a generics

--- src/usuarios/urls.py
+++ src/usuarios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('usuarios/', views.users, name='users_list'),
-    path('usuarios/<int:pk>/', views.user_detail, name='user_detail'),
+    path('usuarios/', views.UsuarioListCreateView.as_view(), name='usuario-list-create'),
+    path('usuarios/<int:pk>/', views.UsuarioDetailView.as_view(), name='usuario-detail'),
 ]
+
--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,32 +1,16 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework import generics
 from .serializers import UsuariosSerializer
 from .models import UsuarioPersonalizado
 
-@api_view(["GET", "POST"])
-def users(request):
-    if request.method == "GET":
-        usuarios = UsuarioPersonalizado.objects.all()
-        serializer = UsuariosSerializer(usuarios, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class UsuarioListCreateView(generics.ListCreateAPIView):
+    queryset = UsuarioPersonalizado.objects.all()
+    serializer_class = UsuariosSerializer
 
-    if request.method == "POST":
-        serializer = UsuariosSerializer(data=request.data)
-        if serializer.is_valid():
-            usuario = serializer.save()
-            return Response(
-                {
-                    "mensaje": "Usuario creado con éxito",
-                    "data": serializer.data
-                }, 
-                status=status.HTTP_201_CREATED
-            )
-            
-        return Response(
-                    {"mensaje": "No se creó porque no es válido"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
+
+class UsuarioDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UsuarioPersonalizado.objects.all()
+    serializer_class = UsuariosSerializer
+
         
 """
 { "username" : "leli" ,
@@ -34,27 +18,3 @@ def users(request):
 "dni": "45592159" ,
 "telefono": "3583417514"} 
 """        
-
-
-@api_view(["GET", "PUT", "DELETE"])
-def user_detail(request, pk):
-    try:
-        usuario = UsuarioPersonalizado.objects.get(pk=pk)
-    except UsuarioPersonalizado.DoesNotExist:
-        return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == "GET":
-        serializer = UsuariosSerializer(usuario)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    elif request.method == "PUT":
-        serializer = UsuariosSerializer(usuario, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == "DELETE":
-        usuario.delete()
-        return Response({"mensaje": "Usuario eliminado"}, status=status.HTTP_204_NO_CONTENT)
-        
